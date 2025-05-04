fd5bdb4ecedc8da806fd873f665cc2d677b0c232
ahmedgamil24 <agemail5050@gmail.com>
2025-05-04 22:56:14 +0300

    created navbar and sidebar successfully

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
-<script setup>
-import { useTheme } from 'vuetify'
-
-const theme = useTheme()
-
-const toggleTheme = () => {
-    theme.global.name.value =
-        theme.global.name.value === 'myCustomLightTheme'
-            ? 'myCustomDarkTheme'
-            : 'myCustomLightTheme'
-}
-</script>
+<script setup></script>
 
 <template>
     <v-app class="bg-error">
         <v-theme-provider with-background>
-            <v-switch @click="toggleTheme"></v-switch>
-
             <router-view />
         </v-theme-provider>
     </v-app>
--- src/Layouts/AdminLayout.vue
+++ src/Layouts/AdminLayout.vue
@@ -1,17 +1,17 @@
 <script setup>
+import { ref } from 'vue'
 import SideBar from '../Components/SideBar.vue'
 import NavBar from '../Components/NavBar.vue'
+
+const drawer = ref(true) // default open
 </script>
 
 <template>
-    <v-app>
-        <v-layout>
-            <side-bar />
-
-            <v-main>
-                <NavBar />
-                <router-view />
-            </v-main>
-        </v-layout>
+    <v-app id="inspire">
+        <SideBar :drawer="drawer" />
+        <v-main>
+            <NavBar @toggle-drawer="drawer = !drawer" />
+            <router-view />
+        </v-main>
     </v-app>
 </template>
